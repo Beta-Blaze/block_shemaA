06e5dccb4d1507d9719ca276f4a5722a360919a3
Alan Shan <55994400+Alan-Shan@users.noreply.github.com>
2021-10-11 12:39:37 +0300

    Add shape types

--- shape.py
+++ shape.py
@@ -1,7 +1,21 @@
+SHAPE_TYPES = {
+    "START": 2,
+    "PROCESS": 4,
+    "INPUT": 5,
+    "IF": 6,
+    "MODIFICATION": 9,
+    "DEFINED_PROCESS": 12,
+    "ONE_PAGE_CONNECTOR": 15,
+    "MULTI_PAGE_CONNECTOR": 16
+}
+
+
 class Shape:
     def __init__(self, shape_id, master, x, y, text=""):
+        if master not in SHAPE_TYPES:
+            print("Illegal shape type")
         self.id = shape_id
-        self.master = master
+        self.master = SHAPE_TYPES[master]
         self.pos = [x, y]
         self.text = text
 
