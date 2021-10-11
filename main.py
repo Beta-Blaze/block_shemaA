ca190eddb243a32f0a4f4a4e5c246aa4a7f0013b
Beta-Blaze <51492936+Beta-Blaze@users.noreply.github.com>
2021-10-11 10:52:57 +0300

    распаковка vsdx

---
+++ main.py
@@ -0,0 +1,24 @@
+import shutil
+import zipfile
+
+
+class Vsdx:
+    def __init__(self, filename):
+        self.filename = filename
+        self.base_filename = self.filename[:-5]
+        self.directory = '\\'.join(__file__.split("\\")[:-1]) + '\\'
+
+    def open_vsdx_file(self):
+        with zipfile.ZipFile(self.filename, "r") as zip_ref:
+            zip_ref.extractall(self.directory + "\\temp")
+
+    def save_vsdx_file(self):
+        shutil.make_archive(self.base_filename, 'zip', self.directory + "\\temp")
+        shutil.move(self.base_filename + '.zip', self.base_filename + '_new.vsdx')
+        shutil.rmtree(self.directory + "\\temp")
+
+
+v = Vsdx("1.vsdx")
+v.open_vsdx_file()
+input()
+v.save_vsdx_file()
