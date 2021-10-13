fc7e162084fae128952350c1c797dba3f725849d
Beta-Blaze <51492936+Beta-Blaze@users.noreply.github.com>
2021-10-13 11:03:17 +0300

    shape_size

--- shape.py
+++ shape.py
@@ -9,17 +9,29 @@ SHAPE_TYPES = {
     "MULTI_PAGE_CONNECTOR": 16
 }
 
-count = 1
+SHAPE_SIZE = {
+    # Height Width
+    "START": [0.3937007874015748, 1.181102362204724],
+    "PROCESS": [0.7874015748031495, 1.181102362204724],
+    "INPUT": [0.7874015748031495, 1.181102362204724],
+    "IF": [0.7874015748031495, 1.181102362204724],
+    "MODIFICATION": [0.7874015748031491, 1.181102362204724],
+    "DEFINED_PROCESS": [0.7874015748031495, 1.181102362204724],
+    "ONE_PAGE_CONNECTOR": [0.3937007874015748, 0.3937007874015748],
+    "MULTI_PAGE_CONNECTOR": [0.4724409448818898, 0.3937007874015749]
+}
+
+id_shape_counter = 1
 
 
 class Shape:
     def __init__(self, master, text=""):
-        global count
+        global id_shape_counter
         if master not in SHAPE_TYPES:
             print("Illegal shape type")
             exit()
-        self.id = count
-        count += 1
+        self.id = id_shape_counter
+        id_shape_counter += 1
         self.master = SHAPE_TYPES[master]
         self.text = text
         self.pos = []
