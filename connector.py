97c7587d04656a10d0daa97e59a64cbff6f88b39
Alan Shan <55994400+Alan-Shan@users.noreply.github.com>
2021-10-13 23:00:06 +0300

    Add support for horizontal connectors

--- connector.py
+++ connector.py
@@ -1,30 +1,36 @@
 import random
+from shape import id_shape_counter
 
 CONNECTOR_WIDTH = 0.25
 
 
 class Connector:
-    def __init__(self, shape_from, shape_to, ):
+    def __init__(self, shape_from, shape_to, connector_position):
         """
         :param shape_from: Shape object
+        :param connector_position: true - vertical; false - horizontal
         :param shape_to: Shape object
         """
         self.from_id = shape_from.id
         self.to_id = shape_to.id
-        self.begin_x = shape_from.pos[0]
-        self.begin_y = shape_from.pos[1]
-        self.end_x = shape_to.pos[0]
-        self.end_y = shape_to.pos[0]
+        self.begin_x = shape_from.pos[0] if connector_position[0] else shape_from.pos[1] - shape_from.size[0] / 2
+        self.begin_y = shape_from.pos[1] - shape_from.size[0] / 2 if connector_position[0] else shape_from.pos[1]
+        self.end_x = shape_to.pos[0] if connector_position[1] else shape_to.pos[1] + shape_to.size[0] / 2
+        self.end_y = shape_to.pos[1] + shape_to.size[0] / 2 if connector_position[1] else shape_to.pos[1]
         self.height = self.end_y - self.begin_y
+        self.pin_x = self.begin_x if connector_position[0] else shape_from.pos[0] - (shape_from.pos[0] - shape_to.pos[0]) / 2
+        self.pin_y = shape_from.pos[1] - (shape_from.pos[1] - shape_to.pos[1]) / 2 if connector_position[0] else self.begin_y
 
     def get_xml(self):
-        return f"""<Shape ID='{random.randint(2000, 30000)}' Type='Shape' Master='20'>
-                        <Cell N='PinX' V='{self.begin_x}'/>
-                        <Cell N='PinY' V='{self.begin_y}'/>
+        global id_shape_counter
+        id_shape_counter += 1
+        return f"""<Shape ID='{id_shape_counter}' Type='Shape' Master='11'>
+                        <Cell N='PinX' V='{self.pin_x}'/>
+                        <Cell N='PinY' V='{self.pin_y}'/>
                         <Cell N='Width' F='GUARD({CONNECTOR_WIDTH}DL)'/>
+                        <Cell N='Height' V = '{self.height}' F='GUARD(EndY-BeginY)'/>
                         <Cell N='LocPinX' V='{CONNECTOR_WIDTH * 0.5}'/>
-                        <Cell N='Height' F='GUARD(EndY-BeginY)'/>
-                        <Cell N='LocPinY' V='{(self.end_y - self.begin_y) * 0.5}'/>
+                        <Cell N='LocPinY' V='{round(self.height * 0.5, 15)}'/>
                         <Cell N='BeginX' V='{self.begin_x}' />
                         <Cell N='BeginY' V='{self.begin_y}' />
                         <Cell N='EndX' V='{self.end_x}' />
@@ -34,15 +40,17 @@ class Connector:
                         <Section N='Control'>
                             <Row N='TextPosition'>
                                 <Cell N='X' V='{CONNECTOR_WIDTH * 0.5}'/>
-                                <Cell N='Y' V='{(self.end_y - self.begin_y) * 0.5}'/>
+                                <Cell N='Y' V='{self.height * 0.5}'/>
                                 <Cell N='XDyn' V='{CONNECTOR_WIDTH * 0.5}' />
-                                <Cell N='YDyn' V='{(self.end_y - self.begin_y) * 0.5}' />
+                                <Cell N='YDyn' V='{self.height * 0.5}' />
                             </Row>
                         </Section>
                         <Section N='Geometry' IX='0'>
-                            <Row T='MoveTo' IX='1'> <Cell N='X' V='{CONNECTOR_WIDTH * 0.5}'/></Row>
-                            <Row T='LineTo' IX='2'><Cell N='X' V='{CONNECTOR_WIDTH * 0.5}'/>
-                                <Cell N='Y' V='{(self.end_y - self.begin_y) * 0.1}'/>
+                            <Row T='MoveTo' IX='1'> 
+                                <Cell N='X' V='{CONNECTOR_WIDTH * 0.5}'/></Row>
+                            <Row T='LineTo' IX='2'>
+                                <Cell N='X' V='{CONNECTOR_WIDTH * 0.5}'/>
+                                <Cell N='Y' V='{self.height}'/>
                             </Row>
                             <Row T='LineTo' IX='3' Del='1'/>
                         </Section>
