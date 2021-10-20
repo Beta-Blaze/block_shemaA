b4f798dfb2bb2044c3c2f80c563972ff879997f1
Alan Shan <55994400+alan-shan@users.noreply.github.com>
2021-10-21 00:34:17 +0300

    Include formatter.exe and add variables parser method.

--- parser.py
+++ parser.py
@@ -1,3 +1,8 @@
+import os
+import subprocess
+import re
+
+
 class Parser:
     def __init__(self, data=''):
         self.data = data
@@ -6,6 +11,7 @@ class Parser:
         self.parse_defines_without_brackets = []
 
     def read(self, name):
+        subprocess.Popen(f".\\helpers\\formatter.exe --i --style=Google {name}")
         with open(name) as f:
             self.data = f.readlines()
 
@@ -52,11 +58,26 @@ class Parser:
             temp.insert(0, i2)
             self.parse_defines_with_brackets.append([name, perem, temp])
 
+    def parse_variables(self, string: str) -> list[str, str | None] | None:
+        keywords = "bool char wchar_t char8_t char16_t char32_t int short long signed unsigned float double".split()
+        if any([string.startswith(i) for i in keywords]):
+            for keyword in keywords:
+                string = string.replace(keyword, "")
+            if re.match(r"[A-z]*?;", string.replace(" ", "")):
+                return [string.replace(" ", "").replace(";", ""), None]
+            initialization = re.search(r'[{].*?[}]', string)
+            if initialization:
+                return [string[0:initialization.start()].replace(" ", ""), string[initialization.start()+1:initialization.end()-1] if string[initialization.start()+1:initialization.end()-1] else None]
+            if re.search(r"[A-z]*? = .*?"):
+                var = string.split("=")
+                return [string[0].replace(" ", ""), string[1].lstrip()]
+
 
 p = Parser()
 p.read('primer.cpp')
 p.prepare()
 # print(*p.data, sep='\n')
 p.define()
-print(p.parse_defines_with_brackets)
-print(p.parse_defines_without_brackets)
+# print(p.parse_defines_with_brackets)
+# print(p.parse_defines_without_brackets)
+print(p.parse_variables("int a;"))
