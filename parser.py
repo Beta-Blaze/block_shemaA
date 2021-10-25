407f3dd312a27d85cf5911c42f926524f65e6aea
Beta-Blaze <51492936+Beta-Blaze@users.noreply.github.com>
2021-10-25 03:23:42 +0300

    final define replace

--- parser.py
+++ parser.py
@@ -55,12 +55,27 @@ class Parser:
             temp = string + temp
             self.parse_defines_with_brackets[name] = [list(map(lambda x: x + '_SUPPER_PUPER_DEF', perem)), temp]
 
+    def find_define_in_string(self, string: str, name: str, shift=0) -> list:
+        res = re.search(r".*?{}\((.+)\)+".format(name), string)
+        if res:
+            pos = string.index(res.group(1))
+            pos = [pos + shift, pos + len(res.group(1)) + shift]
+            shift = pos[0]
+            return [pos] + self.find_define_in_string(res.group(1), name, shift)
+        return []
+
     def replace_define(self, string: str) -> str:
         for i in self.parse_defines_with_brackets:
-            inits = re.finditer(r".*?{}\((.+)\)+".format(i), string)  # TODO
-            for init in inits:
-                perem = init.group(1)
-                print(perem)
+            pos = self.find_define_in_string(string, i)
+            if pos:
+                while pos:
+                    pos = pos[-1]
+                    perem = string[pos[0]: pos[1]].split(', ')
+                    new = self.parse_defines_with_brackets[i][1]
+                    for p in range(len(self.parse_defines_with_brackets[i][0])):
+                        new = new.replace(self.parse_defines_with_brackets[i][0][p], perem[p])
+                    string = string.replace(f'{i}({string[pos[0]: pos[1]]})', new)
+                    pos = self.find_define_in_string(string, i)
         return string
 
     def parse_variable_initializations(self, string: str) -> list:
