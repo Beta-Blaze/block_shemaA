e6800cf8dd5d1c799b8bbc4ad4acb803dc0182ea
Alan Shan <55994400+alan-shan@users.noreply.github.com>
2022-07-05 21:21:08 +0300

    Small changes to incorrect POSt handling.

--- web/visio/views.py
+++ web/visio/views.py
@@ -5,11 +5,14 @@ from .forms import CodeInputForm
 
 
 def index(request):
+    exception = None
     if request.method == 'POST':
         form = CodeInputForm(request.POST, request.FILES)
         if form.is_valid():
             return HttpResponseRedirect('/thanks/')
+        else:
+            exception = "Форма заполнена некорретно, либо прикрепленный файл - не код"
     else:
         form = CodeInputForm()
 
-    return render(request, 'code_input.html', {"form": form})
+    return render(request, 'code_input.html', {"form": form, "exception": exception})
